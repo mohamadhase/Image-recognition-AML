004bf4d8863533ba239d874cffe4751efe5b7c05
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-03-12 18:13:07 +0200

    move the contant path to constants file and fix the name

--- back-end/Recognition/__init__.py
+++ back-end/Recognition/__init__.py
@@ -6,9 +6,8 @@ from fastapi import FastAPI,Request
 from http import HTTPStatus
 # Local imports
 from Recognition.src.classes import GoogleVisionClient
-
+from Recognition.src.constants import CREDENTIALS_PATH
 # Set the credentials file path
-credentials_path = '../back-end/training2-project-948749066640.json'
 
 app = FastAPI(
     title="Image Recognition API",
@@ -19,5 +18,5 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='Recognition/logs/ImageRecognition.log',level=logging.DEBUG)
 
 # Create google vision client instance
-client = GoogleVisionClient.get_instance(credentials_path).google_client
+client = GoogleVisionClient.get_instance(CREDENTIALS_PATH).google_client
 from Recognition.endpoints import api
