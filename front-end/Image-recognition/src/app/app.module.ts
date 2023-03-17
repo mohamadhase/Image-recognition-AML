49bd0c543c8d3533f05af2f7c8d6b603a55eb1db
Dana-Ismail <dana.ismail@stu.najah.edu>
2023-03-17 20:19:45 +0200

    create wordcloud logic

--- front-end/Image-recognition/src/app/app.module.ts
+++ front-end/Image-recognition/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecognitionComponentComponent } from './recognition-component/recognition-component.component';
+import { ResultComponentComponent } from './result-component/result-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecognitionComponentComponent
+    RecognitionComponentComponent,
+    ResultComponentComponent
   ],
   imports: [
     BrowserModule,
